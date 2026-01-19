83c5d08672122540067fc734c871072fcfb174c4
Madhav <your_email@example.com>
2026-01-19 16:44:33 +0530

    Day-1: Python Function Practice

---
+++ functions_practice.py
@@ -0,0 +1,51 @@
+#Check if a num is prime or not
+def PrimeNums(num) :
+    if num < 2:
+        return False
+    
+    for i in range(2, num) :
+        if num % i == 0:
+            return False
+
+    return True
+
+
+#Reverse a String
+def ReverseString(str) :
+    if len(str) < 2:
+        return str
+
+    list_of_str = list(str)
+    list_of_rev_str = []
+    for i in range(len(list_of_str) - 1, -1, -1) :
+        list_of_rev_str.append(list_of_str[i])
+
+    return ''.join(list_of_rev_str)
+
+
+#Count word frequency in a sentence
+def WordFrequency(sentence) :
+    words = sentence.split()
+    freq = {}
+
+    for w in words :
+        freq[w] = freq.get(w, 0) + 1
+
+    for w, c in freq.items():
+        print(w,  ":", c)
+
+    return -1
+
+WordFrequency("mango apple orange mango apple")
+
+
+#Return Second Largest Num in List
+def SecondLargestNum(arr) :
+    MAX_ITEM = 0
+    for i in arr :
+        if i > MAX_ITEM:
+            sec_max = MAX_ITEM
+            MAX_ITEM = i
+        else :
+            continue
+    return sec_max
